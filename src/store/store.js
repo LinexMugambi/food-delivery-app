0d65b6187e468620c9e2e0dca092398fcd2dedb2
git_LinexMugambi <linexmugambi254@gmail.com>
2024-09-18 14:47:38 +0300

    fixed errors

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./shopping-cart/cartSlice";
 import cartUiSlice from "./shopping-cart/cartUiSlice";
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import logger from 'redux-logger';
+import { thunk } from 'redux-thunk'; // Named import
 
 const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
     cartUi: cartUiSlice.reducer,
-    middleware: [thunk, logger]
   },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(thunk, logger), // Correctly configure middleware
 });
 
 export default store;
